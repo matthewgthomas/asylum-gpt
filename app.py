facc939d9a8a6e4bb9a7bc7dc96c5ca19a8a7177
Matthew Gwynfryn Thomas <matthewgthomas@gmail.com>
2023-04-02 20:35:40 +0100

    Use LangChain instead

--- app.py
+++ app.py
@@ -1,66 +1,26 @@
 from flask import Flask, render_template, request
-from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy.sql import text
-import openai
-
-# Replace with your OpenAI API key
-openai.api_key = "your_openai_api_key_here"
+from langchain import OpenAI, SQLDatabase, SQLDatabaseChain
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///asylum.sqlite'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db = SQLAlchemy(app)
 
-# Define your database models here
-# For example:
-# class User(db.Model):
-#     id = db.Column(db.Integer, primary_key=True)
-#     name = db.Column(db.String(100), nullable=False)
-#     ...
+db = SQLDatabase.from_uri("sqlite:///asylum.db")
+llm = OpenAI(temperature = 0, openai_api_key="<Your OpenAI API key goes here>")
+db_chain = SQLDatabaseChain(llm=llm, database=db, verbose=True)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
         query = request.form['query']
-        # Process the query with GPT-4 and SQLite
-        prompt, response, sql_query, result = process_query(query)
-        return render_template('index.html', prompt=prompt, response=response, sql_query=sql_query, result=result)
+
+        # Process the query with GPT-3.5 and LangChain
+        result = process_query(query)
+
+        return render_template('index.html', result=result)
     return render_template('index.html')
 
-def get_database_schema():
-    result = db.session.execute(text("SELECT name FROM sqlite_master WHERE type='table';"))
-    tables = [table[0] for table in result.fetchall()]
-
-    schema = []
-    for table in tables:
-        result = db.session.execute(f"PRAGMA table_info({table});")
-        fields = [column[1] for column in result.fetchall()]
-        schema.append(f"{table} ({', '.join(fields)})")
-
-    return ', '.join(schema)
-
 def process_query(query):
-    database_schema = get_database_schema()
-    prompt = f"Given a SQLite database with the following tables: {database_schema}, process the following natural language query: '{query}'."
-    response = openai.Completion.create(
-        engine="text-davinci-002",  # GPT-4 should be available as an engine
-        prompt=prompt,
-        max_tokens=100,
-        n=1,
-        stop=None,
-        temperature=0.5,
-    )
-
-    sql_query = response.choices[0].text.strip()
-    result = execute_sql(sql_query)
-    return prompt, response, sql_query, result
-
-def execute_sql(sql_query):
-    try:
-        result = db.session.execute(text(sql_query))
-        return result.fetchall()
-    except Exception as e:
-        return str(e)
+    result = db_chain.run(query)
+    return result
 
 if __name__ == '__main__':
     app.run(debug=True)
